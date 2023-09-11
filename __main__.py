15d226c7aead6cc7c651d8e6e39d8bb2822d6e49
netharuM <netharu.m@gmail.com>
2023-09-12 01:58:46 +0530

    Added different encodings support [FIXED:#1]

--- __main__.py
+++ __main__.py
@@ -1,7 +1,6 @@
 #!/bin/python3
 
 # SRT time fixer github:@netharuM/srt-subtitle-time-fix
-
 from srt import srt_time
 from srt import srt_data
 import argparse
@@ -9,13 +8,27 @@ import argparse
 
 def main(args):
     srt_file_name = args.file
-    srt_file_obj = srt_data()
-    srt_file = open(srt_file_name, "r")
-    srt_file_obj.parse_from_str(srt_file.read())
-    srt_file.close()
+    encoding = args.encoding
 
     extra_time = srt_time()
     extra_time.from_str(args.time)
+
+    srt_file_obj = srt_data()
+    with open(srt_file_name, 'rb') as srt_file:
+        try:
+            srt_file_obj.parse_from_str(srt_file.read().decode(encoding))
+        except UnicodeDecodeError:
+            print(f"Error: The encoding ('{encoding}') cannot decode the file")
+            print(
+                'Please provide the --encoding/-e argument with the encoding that the file is using')
+            print('  Ex:')
+            print(
+                f'     srt-stf -f {srt_file_name} -t {args.time} -o {args.out} -e [encoding the file is using]')
+            print(
+                f'     srt-stf -f {srt_file_name} -t {args.time} -o {args.out} -e latin-1')
+            print(
+                f'     srt-stf -f {srt_file_name} -t {args.time} -o {args.out} -e utf-8')
+            exit(1)
 
     if not args.negative:
         for s in srt_file_obj.sequences:
@@ -26,24 +39,25 @@ def main(args):
             s.sub_beginning_time -= extra_time
             s.sub_ending_time -= extra_time
 
-    final_file = open(args.out, "w")
-    final_file.write(srt_file_obj.to_str_file())
-    final_file.close()
+    with open(args.out, "wb") as final_file:
+        final_file.write(srt_file_obj.to_str_file().encode(encoding))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='srt-stf')
     parser.add_argument(
         '--negative', '-n', help='Subtracts the time from the subtitle file making the subtitles appear sooner', action='store_true')
+    parser.add_argument('--encoding', '-e', type=str,
+                        help="The preferred encoding for the file (default is utf-8, which may not work with some files)",
+                        default='utf-8')
     parser.add_argument('--file', '-f', type=str,
-                        help="path to the current subtitle.srt file")
-    parser.add_argument('--out', '-o', type=str, help='output file name')
+                        help="path to the current subtitle.srt file",
+                        required=True)
+    parser.add_argument('--out', '-o', type=str, help='output file name',
+                        required=True)
     parser.add_argument(
-        '--time', '-t', type=str, help='Time to be changed in the format of HH:MM:SS,mmm mmm=Milliseconds HH=hours MM=minutes SS=seconds')
+        '--time', '-t', type=str, help='Time to be changed in the format of HH:MM:SS,mmm mmm=Milliseconds HH=hours MM=minutes SS=seconds',
+        required=True)
 
     args = parser.parse_args()
-    if (args.file == None or args.out == None or args.time == None):
-        print(
-            'Insufficient arguments use --help to see args\n"-f,-o and -t" arguments are required')
-        exit(1)
     main(args)
--- docs/README.md
+++ docs/README.md
@@ -13,25 +13,26 @@ hard code the time with srt-stf
 
 ## Usage
 -   ```
-    usage: srt-stf [-h] [--negative] [--file FILE] [--out OUT] [--time TIME]
+    usage: srt-stf [-h] [--negative] [--encoding ENCODING] --file FILE --out OUT --time TIME
 
     options:
     -h, --help            show this help message and exit
     --negative, -n        Subtracts the time from the subtitle file making the subtitles appear sooner
+    --encoding ENCODING, -e ENCODING
+                            The preferred encoding for the file (default is utf-8, which may not work with some files)
     --file FILE, -f FILE  path to the current subtitle.srt file
     --out OUT, -o OUT     output file name
-    --time TIME, -t TIME  Time to be changed in the format of HH:MM:SS,mmm mmm=Milliseconds HH=hours MM=minutes SS=seconds
     ```
 - examples:
     ```
     # to delay the subtitle by 29 seconds
-    srt-stf -f <input_file_name>.srt -t <TIME> -o <output_file_name>.srt
-    srt-stf -f track.srt -t 00:00:29,000 -o out.srt
-    #              HOURS-----|  |  |   | 
-    #              MINUTES------|  |   | 
-    #              SECONDS---------|   | 
-    #              MILLISECONDS--------|
+    $ srt-stf -f <input_file_name>.srt -t <TIME> -o <output_file_name>.srt
+    $ srt-stf -f track.srt -t 00:00:29,000 -o out.srt -e utf-8
+    #                HOURS-----|  |  |   | 
+    #                MINUTES------|  |   | 
+    #                SECONDS---------|   | 
+    #                MILLISECONDS--------|
 
     # if you wanna play the subtitle 29 seconds sooner. Just add the '-n' flag to the command
-    srt-stf -f track.srt -t 00:00:29,000 -o out.srt -n
+    $ srt-stf -f track.srt -t 00:00:29,000 -o out.srt -n
     ```
